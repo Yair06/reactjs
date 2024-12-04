2151cbc94a68053c071ae6a03b8fe42a9818da60
cohen <yair.cohen@ortmontreuil.fr>
2024-12-04 16:32:06 +0100

    deploy et test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-      <div className="App">
-        <h1>Hello World, je suis Yair !</h1>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world ! je suis Yair
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
